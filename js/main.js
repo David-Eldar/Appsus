83281886e0cd40d8ff325d81899b6ed514a9d08c
David Eldar <doomtak@gmail.com>
2022-06-29 15:33:03 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// const { createApp } = Vue
+import appHeader from "./cmps/app-header.cmp.js";
+import appFooter from "./cmps/app-footer.cmp.js";
+
+import { router } from "./router.js";
+
+
+
+
+
+
+
+const options = {
+    template: `
+        <section class="main-container">
+            <app-header />
+            <router-view/>
+            <app-footer />
+        </section>
+
+ `,
+
+
+
+    components: {
+        appHeader,
+        appFooter,
+
+    },
+
+};
+
+
+
+const app = Vue.createApp(options)
+app.use(router)
+app.mount('#app')
+
+
+
---
+++ js/router.js
@@ -0,0 +1,32 @@
+import keepApp from "./views/book-app.cmp.js";
+import homePage from './views/home-page.cmp.js';
+import aboutPage from './views/about-page.cmp.js';
+
+
+
+const routes = [
+    {
+        path: '/',
+        component: homePage
+    },
+    {
+        path: '/about',
+        component: aboutPage
+    },
+    {
+        path: '/book',
+        component: keepApp
+    },
+    {
+
+    },
+    // {
+    //     path: '/book/add/:bookId?',
+    //     component: bookAdd
+    // },
+]
+
+export const router = VueRouter.createRouter({
+    routes,
+    history: VueRouter.createWebHashHistory()
+})
